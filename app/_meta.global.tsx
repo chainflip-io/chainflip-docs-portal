3e068938b2c293662fdf478f2c369441dc6d1d20
ShaunRocker1994 <shaun@chainflip.io>
2025-12-04 14:12:53 +0100

    Another round of fixes to lending docs w Gabe & Maxim feedback taken into account.

--- app/_meta.global.tsx
+++ app/_meta.global.tsx
@@ -41,8 +41,7 @@ export default {
             },
             'how-to-supply': '',
             'how-to-borrow': '',
-            'understanding-liquidations': '',
-            'soft-vs-hard-liquidations': '',
+            'understanding_liquidations': '',
             'liquidation-examples': '',
             ___: {
                 type: 'separator',
